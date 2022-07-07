33b3b2705fbcdafde7080f869acdc49a3a4a41f7
Mike Dzikowski <micdz@microsoft.com>
2022-07-07 10:20:02 -0400

    clean up params

--- runbooks/Get-SessionHostVirtualMachine.ps1
+++ runbooks/Get-SessionHostVirtualMachine.ps1
@@ -9,7 +9,7 @@ $connectionName = "AzureRunAsConnection"
 try
 {
     # Get the connection "AzureRunAsConnection "
-    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName
 
     Add-AzAccount `
         -ServicePrincipal `
