d59f4afa8c346c1906a048e0831a0d05716ec020
Mike Dzikowski <micdz@microsoft.com>
2022-07-11 13:22:47 -0400

    testing mi inside runbook

--- runbooks/Get-SessionHostVirtualMachine.ps1
+++ runbooks/Get-SessionHostVirtualMachine.ps1
@@ -5,30 +5,16 @@ param (
 )
 
 
-$connectionName = "AzureRunAsConnection"
-try
-{
-    # Get the connection "AzureRunAsConnection "
-    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName
-
-    Add-AzAccount `
-        -ServicePrincipal `
-        -TenantId $servicePrincipalConnection.TenantId `
-        -ApplicationId $servicePrincipalConnection.ApplicationId `
-        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint `
-        -EnvironmentName AzureUSGovernment | Out-Null
- }
-catch {
-    if (!$servicePrincipalConnection)
-    {
-        $ErrorMessage = "Connection $connectionName not found."
-        throw $ErrorMessage
-    } else{
-        Write-Error -Message $_.Exception
-        throw $_.Exception
-    }
+# Connect using a Managed Service Identity
+try {
+    $AzureContext = (Connect-AzAccount -Identity).context
+}
+catch{
+    Write-Output "There is no system-assigned user identity. Aborting.";
+    exit
 }
 
+$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
 
 # Getting the hostpool first
 $hostpool = Get-AzWvdHostPool | Where-Object { $_.Name -eq $hostpoolname }
@@ -45,7 +31,7 @@ $hostpoolRg = ($hostpool).id.split("/")[4]
 # Select a VM in hostpool
 $sessionHosts = Get-AzWvdSessionHost -ResourceGroupName $hostpoolRg -HostPoolName $hostpoolName
 $existingSessionHost = ($sessionHosts.Name.Split("/")[-1]).Split(".")[0]
-$productionVirtualMachine = Get-AzVM -Name $existingSessionHost 
+$productionVirtualMachine = Get-AzVM -Name $existingSessionHost
 
 $objOut = [PSCustomObject]@{
     productionVm = $productionVirtualMachine.Name
